14d1df38eda7e49aaf253d7aeba49c14f9f0e17e
Pierce ONeill <pierceoneillky@gmail.com>
2018-11-16 16:42:22 +0000

    change to django admin header name and babysitter fields

--- babysitters/admin.py
+++ babysitters/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from .models import Babysitter
-from .models import Experience
-
 
 # Register your models here.
 admin.site.register(Babysitter)
-admin.site.register(Experience)
--- babysitters/models.py
+++ babysitters/models.py
@@ -1,32 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
 class Babysitter(models.Model):
+    list_display = ('firstName', 'lastName', 'minderType')
     firstName = models.CharField(max_length=50, blank=True, null=True)
     lastName = models.CharField(max_length=50, blank=True, null=True)
     minderType = models.CharField(max_length=50, blank=True, null=True)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
-    location = models.CharField(max_length=50, blank=True, null=True)
-    bio = models.TextField(blank=True, null=True)
-    twitter = models.CharField(max_length=100, blank=True, null=True)
-    facebook = models.CharField(max_length=100, blank=True, null=True)
-    donator = models.BooleanField(default=False)
-    
-class Experience(models.Model):
-    """This is the model holding the users Experience information"""
-    babysitter = models.ForeignKey(Babysitter, related_name='experience')
-    jobTitle = models.CharField(max_length=50)
-    company = models.CharField(max_length=50)
-    location = models.CharField(max_length=50)
-    dateFrom = models.DateField(auto_now=False, auto_now_add=False)
-    dateTo = models.DateField(
-        auto_now=False, auto_now_add=False, null=True, blank=True)
-    current = models.BooleanField(default=False)
-    
-
-
-def __str__(self):
-    return self.name
+    address1 = models.CharField(max_length=100, null=True)
+    address2 = models.CharField(max_length=100, null=True)
+    city = models.CharField(max_length=20, null=True)
+    county = models.CharField(max_length=100, null=True)
+    eircode = models.CharField(max_length=7, null=True)
+    biography = models.TextField(max_length=280,blank=True)
+    def __str__(self):
+        return self.firstName + ' ' + self.lastName
