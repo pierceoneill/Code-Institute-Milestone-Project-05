cd486c28a0bfae98c7b6e70eb9e4450760a57c98
Pierce ONeill <pierceoneillky@gmail.com>
2018-11-30 01:23:31 +0000

    updatign fields in contact form and creating new toast messages

--- static/js/sendemail.js
+++ static/js/sendemail.js
@@ -2,11 +2,14 @@ function sendMail(contactForm) {
     emailjs.send("gmail", "minderfinder", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "booking_request": contactForm.booking_request.value
+        "booking_request": contactForm.booking_request.value,
+        "mobile": contactForm.mobile.value,
+        "date": contactForm.date.value,
+        "time": contactForm.time.value
     })
     .then(
         function(response) {
-            console.log("SUCCESS", response);
+          messages.success(request, 'Form submission successful')
         },
         function(error) {
             console.log("FAILED", error);
