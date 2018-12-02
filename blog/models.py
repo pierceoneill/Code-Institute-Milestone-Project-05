bb0c4f906bb4bc4a54b2aac1cbad344cebc83612
Pierce ONeill <pierceoneillky@gmail.com>
2018-12-02 01:15:06 +0000

    implementing add comment to blog and other blog styling

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,8 @@ class Post(models.Model):
     image = models.ImageField(upload_to='images', default='Upload Picture')
     category = models.CharField(max_length=200)
     description = models.CharField(max_length=340)
+    views = models.IntegerField(default=0)
+    tag = models.CharField(max_length=30, blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
